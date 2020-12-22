52039bbc27a86dc5883806e4405d8d7c598c8a89
Divya C D <62093093+DivyaBharath-prog@users.noreply.github.com>
2020-12-22 12:41:21 +0530

    Create Program-5.c

---
+++ Program-5.c
@@ -0,0 +1,20 @@
+//Division of two numbers
+
+//Input :10 5
+//Output :2
+
+#include<stdio.h>
+int main()
+{
+      int num1,num2,div;
+      printf("Enter Two Numbers, First Number, Second Number ");
+        scanf("%d %d ", &num1, &num2);
+       div=num1/num2;
+      printf("Division of %d & %d is = %d", div);
+      return 0;
+}
+
+
+
+
+
