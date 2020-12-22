c6341794981808f04d8b30d5ee9c4d193169750f
Divya C D <62093093+DivyaBharath-prog@users.noreply.github.com>
2020-12-22 12:35:03 +0530

    Create Program-1.c

---
+++ Program-1.c
@@ -0,0 +1,25 @@
+Input : 23  34 
+Output: Sum of distances = 57
+
+
+//Program to add two distances (feet-inch)
+#include <stdio.h>
+struct Distance
+{
+    int feet;
+    float inch;
+} dist1, dist2, sum;
+
+int main()
+{
+      printf("Enter feet for 1st and 2nd distance: ");
+    scanf("%d",d", &dist1.feet, &dist2.feet);
+
+    // adding feet
+    sum.feet = dist1.feet + dist2.feet;
+    // adding inches
+
+
+    printf("Sum of feet = %d”,sum.feet);
+    
+}
