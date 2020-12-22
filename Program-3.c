cf4b32354d8067253eae8baf5eac509304e42ece
Divya C D <divyadv.dv27@gmail.com>
2020-12-22 08:42:50 +0000

    sum

--- Program-3.c
+++ Program-3.c
@@ -2,21 +2,18 @@
 //Input: 10 20 
 //Output: 30
 #include<stdio.h>
-#include<conio.h>
 struct sum
 {
 int a;
 int b;
 };
-void main()
+int main()
 {
 int sum1;
 struct sum s;
-clrscr();
-printf(“Enter two numbers:”);
-scanf(“%d%d”,&s.a,&s.b);
+printf("Enter two numbers:");
+scanf("%d %d",&s.a,&s.b);
 sum1=s.a+s.b;
-printf(“nSum=%d”,sum1);
-getch();
+printf("Sum=%d",sum1);
 }
 
